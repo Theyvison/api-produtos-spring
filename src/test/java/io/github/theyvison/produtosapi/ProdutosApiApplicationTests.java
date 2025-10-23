7e1e785effd8dd11895428b5d309521598989d10
Theyvison <theyvison.jf@hotmail.com>
2025-10-23 13:40:59 -0300

    first commit

---
+++ src/test/java/io/github/theyvison/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.theyvison.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
