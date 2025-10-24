9ed3a6389a8ac0a97b415e178892c92c7936280d
Theyvison <theyvison.jf@hotmail.com>
2025-10-24 14:46:03 -0300

    Criando o mapeamento JPA para entidade Produto

--- src/main/java/io/github/theyvison/produtosapi/model/Produto.java
+++ src/main/java/io/github/theyvison/produtosapi/model/Produto.java
@@ -1,9 +1,19 @@
 package io.github.theyvison.produtosapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Produto {
+    @Id
+    @Column
     private String id;
+    @Column
     private String nome;
+    @Column
     private String descricao;
+    @Column
     private Double preco;
 
     public String getId() {
